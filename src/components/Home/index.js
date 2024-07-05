e1ee579883ee76b62b881e859ff43b7c4cc8353f
saitejadattu <bodasaiteja@gmail.com>
2024-07-05 05:02:30 +0000

    commit message

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -92,25 +92,25 @@ class Home extends Component {
           return (
             <>
               {videoList.length > 0 ? (
-                <ul className="video-unordered-list">
+                <ul className='video-unordered-list'>
                   {videoList.map(each => (
                     <VideoItem each={each} key={each.id} />
                   ))}
                 </ul>
               ) : (
                 <Div
-                  height="100vh"
-                  display="flex"
-                  fd="column"
-                  jc="center"
-                  ai="center"
-                  as="center"
+                  height='100vh'
+                  display='flex'
+                  fd='column'
+                  jc='center'
+                  ai='center'
+                  as='center'
                 >
                   <Div>
                     <Image
-                      height="300px"
-                      src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-search-results-img.png"
-                      alt="no videos"
+                      height='300px'
+                      src='https://assets.ccbp.in/frontend/react-js/nxt-watch-no-search-results-img.png'
+                      alt='no videos'
                     />
                   </Div>
                   <Heading color={isDark ? 'white' : '#1e293b'}>
@@ -120,14 +120,14 @@ class Home extends Component {
                     Try different key words or remove search filter
                   </Para>
                   <Button
-                    bgColor="#4f46e5"
-                    bw="0"
-                    br="5"
-                    as="center"
-                    p="10"
-                    fw="500"
-                    width="100px"
-                    color="white"
+                    bgColor='#4f46e5'
+                    bw='0'
+                    br='5'
+                    as='center'
+                    p='10'
+                    fw='500'
+                    width='100px'
+                    color='white'
                     onClick={this.getAllVideos}
                   >
                     Retry
@@ -148,8 +148,8 @@ class Home extends Component {
   failureView = () => <FailureView getRetry={this.getRetry} />
 
   inProgressView = () => (
-    <div className="loading-container" data-testid="loader">
-      <Loader type="ThreeDots" color="#4f46e5" width={50} height={50} />
+    <div className='loading-container' data-testid='loader'>
+      <Loader type='ThreeDots' color='#4f46e5' width={50} height={50} />
     </div>
   )
 
@@ -176,45 +176,45 @@ class Home extends Component {
           return (
             <Dov
               bgColor={isDark ? '#181818' : '#f9f9f9'}
-              height="100%"
-              fd="column"
-              jc="space-between"
-              data-testid="home"
+              height='100%'
+              fd='column'
+              jc='space-between'
+              data-testid='home'
             >
               <Header />
-              <div className="sidebar-video-container">
+              <div className='sidebar-video-container'>
                 <MobDiv>
                   <SideBar />
                 </MobDiv>
                 <div>
                   {isBanner && (
                     <Div
-                      bi="https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png"
-                      bSize="cover"
-                      width="175vh"
-                      height="55vh"
-                      p="20"
-                      data-testid="banner"
+                      bi='https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png'
+                      bSize='cover'
+                      width='175vh'
+                      height='55vh'
+                      p='20'
+                      data-testid='banner'
                     >
-                      <div className="banner-inside-container">
-                        <div className="banner-text-container">
+                      <div className='banner-inside-container'>
+                        <div className='banner-text-container'>
                           <img
-                            className="nxtImage"
-                            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-                            alt="nxt watch logo"
+                            className='nxtImage'
+                            src='https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+                            alt='nxt watch logo'
                           />
                           <p>
                             Buy Nxt Watch Premium Prepaid Plans With <br /> UPI
                           </p>
-                          <button type="button" className="getitnow-button">
+                          <button type='button' className='getitnow-button'>
                             GET IT NOW
                           </button>
                         </div>
-                        <div className="close-container">
+                        <div className='close-container'>
                           <button
-                            type="button"
-                            className="close-button"
-                            data-testid="close"
+                            type='button'
+                            className='close-button'
+                            data-testid='close'
                             onClick={this.onClickGamingCloseBanner}
                           >
                             <IoMdClose />
@@ -224,12 +224,12 @@ class Home extends Component {
                     </Div>
                   )}
                   <div>
-                    <div className="search-input-container">
+                    <div className='search-input-container'>
                       <input
-                        type="search"
+                        type='search'
                         value={searchInput}
                         className={isDark ? 'darksearch-input' : 'search-input'}
-                        placeholder="Search"
+                        placeholder='Search'
                         onChange={this.onChangeSearchInput}
                       />
                       <div
@@ -240,13 +240,13 @@ class Home extends Component {
                         }
                       >
                         <Button
-                          bw="0"
-                          bc="transparent"
-                          as="center"
-                          data-testid="searchButton"
+                          bw='0'
+                          bc='transparent'
+                          as='center'
+                          data-testid='searchButton'
                           onClick={this.onClicksearch}
                         >
-                          <ImSearch className="search-icon" />
+                          <ImSearch className='search-icon' />
                         </Button>
                       </div>
                     </div>
